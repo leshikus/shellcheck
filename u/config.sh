466ef51ef5d521916448e7e1d8e3b76f7f1faba7
alexei.fedotov <alexei.fedotov@dfe85002-abae-14f5-80a1-d8df27ef3c54>
2010-12-19 14:45:42 +0000

    Improved environment passing

--- u/config.sh
+++ u/config.sh
@@ -17,6 +17,13 @@ function quote_space() {
   sed -e 's/ /\\ /g'
 }
 
+function pass_env() {
+  for e in $@
+  do
+    eval "echo $e=\${$e}"
+  done
+}
+
 #   Working directories
 # DDIR this file script location
 # QDIR the quoted version of DDIR
@@ -32,6 +39,8 @@ fgrep "$ERR_MESSAGE" "$DDIR"/config.sh || error "$ERR_MESSAGE"
 
 # Run an upper level config if any
 USER=${USER:-hudson}
+CONFIG='SIMULATOR TARGET_CC TARGET_AS TARGET_LD RESULT_DIR'
+
 if test -f "$DDIR"/../config.sh
 then
   . "$DDIR"/../config.sh
@@ -63,10 +72,8 @@ test "$CLEAN_ENV" = true || exec env -i CLEAN_ENV=true \
   CPPFLAGS="-I$QDIR/usr/include" \
   LD_LIBRARY_PATH="$DDIR/usr/lib" \
   SVN_SSH="ssh -p $SSH_PORT" \
-  SIMULATOR=${SIMULATOR} \
-  TARGET_CC=${TARGET_CC} \
-  RESULT_DIR=${RESULT_DIR} \
   SSH_AUTH_SOCK="$SSH_AUTH_SOCK" \
   SSH_AGENT_PID="$SSH_AGENT_PID" \
+  `pass_env $CONFIG` \
   sh -evx "$0" "$@"
 
