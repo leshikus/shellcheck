a1e935870b9774d429d6ca2c2dd7ae25efcf5231
alexei.fedotov <alexei.fedotov@dfe85002-abae-14f5-80a1-d8df27ef3c54>
2011-01-20 19:51:42 +0000

    Cleaned Hudson environment so it would no longer rewrite imported variables

--- u/config.sh
+++ u/config.sh
@@ -7,26 +7,13 @@ GNU_URL=${GNU_URL:-http://mirrors.kernel.org/gnu}
 HUDSON_URL=http://hudson.gotdns.com/latest/hudson.war
 
 #
-# Detect errors
-#
-set -e -o pipefail
-
-#
 # Load functions
 #
 DDIR=`cd "$DDIR"; pwd -P`
 . "$DDIR"/util.sh
 
-make_working_directories
-
 #
-# Set user environment
-#
-USER=${USER:-hudson}
-HOME=${HOME:-/home/hudson}
-
-#
-# Allowed environment, other vars are reset
+# Import shell environment
 #
 CONFIG='
 USER
@@ -40,6 +27,7 @@ SIMULATOR
 TARGET_CC
 TARGET_AS
 TARGET_LD
+JAVA_HOME
 JOB
 TIMESTAMP
 WORKSPACE_DIR
@@ -49,34 +37,19 @@ SVN_SSH
 SSH_AUTH_SOCK
 SSH_AGENT_PID'
 
-# Run an upper level config if any
-if test -f "$DDIR"/../config.sh
-then
-  . "$DDIR"/../config.sh
-fi
-  
+
+#
+# Set user environment
+#
+USER=${USER:-hudson}
+HOME=${HOME:-/home/hudson}
+
+prepare_env
+
 # Calculated varaibles
 SSH_ID=${SSH_ID:-$USER@$SSH_SERVER}
 SSH_PORT=${SSH_PORT:-22}
 SVN_SSH="ssh -p $SSH_PORT"
-
-uname -a # System
-ARCH=`arch`
-if test "$ARCH" = i686
-then
-  ARCH_BITS=32
-elif test "$ARCH" = x86_64
-then
-  ARCH_BITS=64 
-fi
-
-#
-# Tool settings
-#
-PATH="$DDIR/usr/bin:$DDIR/bin:/bin:/usr/bin"
-LDFLAGS="-L$QDIR/usr/lib"
-CPPFLAGS="-I$QDIR/usr/include"
-LD_LIBRARY_PATH="$DDIR/usr/lib"
 
 restart_clean_env "$@"
 
