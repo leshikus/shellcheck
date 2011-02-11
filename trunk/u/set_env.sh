a1b83328c5f3b9c10523fbcc48f6669a8b39c229
alexei.fedotov <alexei.fedotov@dfe85002-abae-14f5-80a1-d8df27ef3c54>
2011-02-11 07:51:29 +0000

    Added remote commit check

--- trunk/u/set_env.sh
+++ trunk/u/set_env.sh
@@ -20,16 +20,11 @@ function apply_to_env_config() {
 function set_job_env() {
   DDIR=`readlink -f "$DDIR"`
   SCRIPT=`readlink -f "$0"`
+  SCRIPT_DIR=`dirname "$SCRIPT"`
+  SCRIPT_DIR_NAME=`basename "$SCRIPT_DIR"`
   JOB=`basename "$0" .sh`
 
-  HUDSON_HOME="$DDIR/../hudson_home"
-  if test "$JOB" = buildtasks
-  then
-    JDIR=`dirname "$SCRIPT"`
-    JOB=`basename "$JDIR"`
-  else
-    JDIR="$HUDSON_HOME/jobs/$JOB"
-  fi
+  test "$JOB" != buildtasks || JOB="$SCRIPT_DIR_NAME"
 }
 
 function restart_if_needed() {
@@ -57,12 +52,10 @@ function add_config_env() {
 function restart_clean_env() {
   local jobstamp=${JOB}_`get_timestamp`
   RESULT_DIR="${RESULT_DIR:-$TMP_DIR/$USER/result/$jobstamp}"
-  WORKSPACE_DIR="${WORKSPACE_DIR:-$JDIR/workspace}"
 
-  mkdir -p "$RESULT_DIR" "$WORKSPACE_DIR"
+  mkdir -p "$RESULT_DIR"
   TMP_DIR=`readlink -f "$TMP_DIR"`
   RESULT_DIR=`readlink -f "$RESULT_DIR"`
-  WORKSPACE_DIR=`readlink -f "$WORKSPACE_DIR"`
 
   local log="$RESULT_DIR/$jobstamp.log"
   local start="$RESULT_DIR/$JOB"
@@ -75,22 +68,35 @@ function restart_clean_env() {
 }
 
 function run_upper_level_config() {
-  if test -f "$DDIR"/../config.sh
-  then
-    . "$DDIR"/../config.sh
-  fi
+  test ! -f "$DDIR"/../config.sh || . "$DDIR"/../config.sh
 }
 
 function set_working_directories() {
-  TMP_JOB_DIR="$TMP_DIR/$USER/$JOBSTAMP"
-  BUILD_DIR="$TMP_JOB_DIR"/build
+  mkdir "$TMP_DIR"/rm
+  mv `ls -1ct "$TMP_DIR"/* "$TMP_DIR"/*/* | sed '1,32d'` "$TMP_DIR"/rm 2>/dev/null || true
+  rm_dir "$TMP_DIR"/rm
 
-  mkdir -p "$TMP_DIR"/dist "$TMP_DIR"/downloads "$TMP_DIR"/tools/bin "$BUILD_DIR" "$TMP_JOB_DIR"
+  TMP_JOB_DIR="$TMP_DIR/$USER/$JOBSTAMP"
+  unset JOBSTAMP
+
+  BUILD_DIR="$TMP_JOB_DIR"/build
+  CUR_DIR=`pwd -P`
+  CUR_DIR_NAME=`basename "$CUR_DIR"`
+
+  HUDSON_HOME="$DDIR"/../hudson_home
+  JDIR="$HUDSON_HOME/jobs/$JOB"
+
+  if test -d "$JDIR"
+  then relink_job_workspace $JOB
+  else JDIR="$SCRIPT_DIR"
+  fi
+
+  WORKSPACE_DIR="${WORKSPACE_DIR:-$JDIR/workspace}"
+  mkdir -p "$TMP_DIR"/dist "$TMP_DIR"/downloads "$TMP_DIR"/tools/bin "$BUILD_DIR" "$TMP_JOB_DIR" "$RESULT_DIR" "$WORKSPACE_DIR"
+  WORKSPACE_DIR=`readlink -f "$WORKSPACE_DIR"`
 
   # removable
-  chmod a+w "$TMP_DIR"/dist "$TMP_DIR"/downloads "$TMP_DIR"/tools "$BUILD_DIR" "$TMP_DIR/$USER" "$RESULT_DIR" "$WORKSPACE_DIR"
-
-  relink_job_workspace $JOB
+  chmod a+w "$TMP_DIR"/dist "$TMP_DIR"/downloads "$TMP_DIR"/tools "$BUILD_DIR" "$TMP_DIR/$USER" "$RESULT_DIR" "$WORKSPACE_DIR" || true
 }
 
 function set_tool_env() {
@@ -106,6 +112,7 @@ function set_tool_env() {
   SSH_ID=${SSH_ID:-$USER@$SSH_SERVER}
   SVN_SSH="ssh -p $SSH_PORT"
 
+  KEY_NAME="$HOME"/.ssh/id_rsa
   export PATH LANG CCFLAGS CPPFLAGS LDFLAGS LD_LIBRARY_PATH SVN_SSH
 }
 
--- trunk/u/setup.sh
+++ trunk/u/setup.sh
@@ -2,26 +2,6 @@
 
 . "$DDIR"/config.sh
 
-#
-# Settings
-#
-KEY_NAME="$HOME"/.ssh/id_rsa
-
-#
-# Functions
-#
-function generate_ssh_keypair() {
-
-  test -f "$KEY_NAME" ||
-    ssh-keygen -N "" -f "$KEY_NAME"
-}
-
-function check_server() {
-  cat "$KEY_NAME".pub |
-    ssh -p $SSH_PORT $SSH_ID \
-    'read key; grep -Fx "$key" $HOME/.ssh/authorized_keys2 || echo "$key" >>$HOME/.ssh/authorized_keys2'
-}
-
 function install_plugins {
   local plugins_dir=$HUDSON_HOME/plugins/
   
@@ -32,8 +12,7 @@ function install_plugins {
 #
 # Executions starts here
 #
-generate_ssh_keypair
-check_server
+check_ssh
 hudson_hide_sensitive_data
 
 case " $* " in
